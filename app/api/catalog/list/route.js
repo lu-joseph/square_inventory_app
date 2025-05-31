321f99430319d37642ef80013ecd25d346ed074a
Joseph Lu <josephlu414@gmail.com>
2025-05-31 02:22:00 -0700

    removed wrong export

--- app/api/catalog/list/route.js
+++ app/api/catalog/list/route.js
@@ -39,49 +39,6 @@ const client = new SquareClient({
     token: process.env.SQUARE_API_KEY,
 });
 
-export default async function handler(req, res) {
-    try {
-        const categoryResponse = await client.catalog.list({
-            types: 'CATEGORY',
-        });
-
-        const categories = new Map(categoryResponse.response.objects.map((category) => ([category.id, category.categoryData.name])))
-
-        const inventoryResponse = await client.inventory.batchGetCounts({
-            locationIds: [
-                process.env.LOCATION_ID,
-            ]
-        })
-
-        const inventoryMap = new Map(inventoryResponse.response.counts.map((count) => ([count.catalogObjectId, count.quantity])));
-
-        const { response } = await client.catalog.list({
-            types: 'ITEM',
-        });
-        const items = response.objects.map((item) => (
-            {
-                itemId: item.id,
-                name: item.itemData.name,
-                variations: item.itemData.variations.map((variation) => (
-                    {
-                        name: variation.itemVariationData.name,
-                        sku: variation.itemVariationData.sku,
-                        variationId: variation.id,
-                        stock: (inventoryMap.get(variation.id) || 0) - (athome_stock.get(variation.itemVariationData.sku) || 0)
-                    }
-                )),
-                categories: item.itemData.categories.map((category) => (categories.get(category.id)))
-            }
-        ))
-        res.json(items);
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Failed to get whole catalog' });
-    }
-}
-
-
 export async function GET() {
     try {
         const categoryResponse = await client.catalog.list({
