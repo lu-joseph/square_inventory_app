29abfafba7a2405d6d66af3d5bb720845b3c72df
Joseph Lu <josephlu414@gmail.com>
2025-05-31 02:20:22 -0700

    nextresponse update

--- app/api/catalog/list/route.js
+++ app/api/catalog/list/route.js
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server';
+
 // server.js
 import { SquareClient, SquareEnvironment } from 'square';
 
@@ -77,4 +79,48 @@ export default async function handler(req, res) {
         console.error(err);
         res.status(500).json({ error: 'Failed to get whole catalog' });
     }
+}
+
+
+export async function GET() {
+    try {
+        const categoryResponse = await client.catalog.list({
+            types: 'CATEGORY',
+        });
+
+        const categories = new Map(categoryResponse.response.objects.map((category) => ([category.id, category.categoryData.name])))
+
+        const inventoryResponse = await client.inventory.batchGetCounts({
+            locationIds: [
+                process.env.LOCATION_ID,
+            ]
+        })
+
+        const inventoryMap = new Map(inventoryResponse.response.counts.map((count) => ([count.catalogObjectId, count.quantity])));
+
+        const { response } = await client.catalog.list({
+            types: 'ITEM',
+        });
+        const items = response.objects.map((item) => (
+            {
+                itemId: item.id,
+                name: item.itemData.name,
+                variations: item.itemData.variations.map((variation) => (
+                    {
+                        name: variation.itemVariationData.name,
+                        sku: variation.itemVariationData.sku,
+                        variationId: variation.id,
+                        stock: (inventoryMap.get(variation.id) || 0) - (athome_stock.get(variation.itemVariationData.sku) || 0)
+                    }
+                )),
+                categories: item.itemData.categories.map((category) => (categories.get(category.id)))
+            }
+        ))
+        return NextResponse.json(items);
+
+    } catch (err) {
+        console.error(err);
+        return NextResponse.status(500).json({ error: 'Failed to get whole catalog' });
+    }
+
 }
